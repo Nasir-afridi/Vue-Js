3c2e8892583abed5d4ceb601eeec3d839bfd7e9b
Nasir-afridi <nasirwc05@gmail.com>
2025-08-14 19:29:47 +0500

    Text interpolation added

---
+++ Vue learning/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>
+        Hello World {{ myMessage }}
+        {{ 2 + 2 }} = {{ 22 + 22 }}
+        <br>
+        {{ add(2, 4)}}
+    </h1>
+</template>
+
+<script setup>
+    const myMessage = 'Yellow Color';
+    function add (a, b ) {
+        return a + b
+    }
+</script>
+
+<style scoped></style>
