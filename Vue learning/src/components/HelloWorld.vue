32d39e6fdf2ac1e0c866bae41efcd1212aebfc34
Nasir-afridi <nasirwc05@gmail.com>
2025-08-14 22:36:55 +0500

    changes added

--- Vue learning/src/components/HelloWorld.vue
+++ Vue learning/src/components/HelloWorld.vue
@@ -1,22 +1,31 @@
 <template>
-    <h1>
+
+
+    <!-- 01-Text interpolation -->
+     <h1>     
         Hello World {{ myMessage }}
         {{ 2 + 2 }} = {{ 22 + 22 }}
         <br>
         {{ add(2, 4)}}
     </h1>
 
-     <a :href="myChannel">Youtube channel</a>       <!--attribute binding : for making the variable dynamic -->
+    <!-- 02-attribute binding : for making the variable dynamic -->
+     <a :href="myChannel">Youtube channel</a>       
      <img :src="Image" :width="ImageWidth" :height="ImageHeight" >
+
+
 </template>
+
 
 <script setup>
 
+    // 01 - Text interpolation
     const myMessage = 'Yellow Color';
     function add (a, b ) {
         return a + b
     }
 
+    // 02 - Attribute bindings Variable: 
     const myChannel = "https://www.youtube.com/watch?v=NcnfsE1B51o&list=PLEPye7A7EcQaJupAt4yTtJx3K-uUuj8si&index=3&ab_channel=SuperSimpleDev"
     const Image = "/favicon.ico"
     const ImageWidth = 400
