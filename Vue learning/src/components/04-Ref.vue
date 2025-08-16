57b042064b740ac2a7fb01d6bb6d0c8a046d91a0
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 17:37:52 +0500

    update the name and the age of the user using ref

--- Vue learning/src/components/04-Ref.vue
+++ Vue learning/src/components/04-Ref.vue
@@ -5,6 +5,22 @@
 
     // 2.
     let userName = ref('')
+
+    // 3.
+    let obj = ref({
+        names: "Nasir",
+        age : 30
+    })
+
+    function nameChange() {
+        if(obj.value.names === "Nasir" && obj.value.age === 30) {
+            obj.value.names = "ali"
+            obj.value.age= 20
+        }else {
+            obj.value.names= "Nasir",
+           obj.value.age = 30
+        }
+    }
 </script>
 
 <template>
@@ -17,6 +33,13 @@
     <!-- 2 -->
      <h1>UserName is {{ userName }}</h1>
     <button @click="userName = 'Nasir'">Add Name</button>
+
+
+    <!-- 3 -->
+     <h1>User name is : {{ obj.names}}</h1>
+     <h1>User age is : {{ obj.age }}</h1>
+
+     <button @click="nameChange">change the user</button>
 </template>
 
 <style scoped></style>
