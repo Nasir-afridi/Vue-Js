6a70be8fcef575b69fb1b329f1e9d4565a60c3ea
Nasir-afridi <nasirwc05@gmail.com>
2025-08-17 00:41:26 +0500

    added the v-for and print the users of array

---
+++ Vue learning/src/components/08-vFor.vue
@@ -0,0 +1,12 @@
+
+<script setup>
+    import { ref } from 'vue';
+
+    let people = ref(["Nasir", "Ali"]);
+</script>
+
+<template>
+    <p v-for="person in people">{{ person }}</p>
+</template>
+
+<style scoped></style>
