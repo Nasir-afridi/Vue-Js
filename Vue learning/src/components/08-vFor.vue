f098401548a4bd2978493d911f078a232c254707
Nasir-afridi <nasirwc05@gmail.com>
2025-08-17 00:51:36 +0500

    added the :key on the values for index

--- Vue learning/src/components/08-vFor.vue
+++ Vue learning/src/components/08-vFor.vue
@@ -1,12 +1,19 @@
 
 <script setup>
+    // Example : 1
     import { ref } from 'vue';
-
     let people = ref(["Nasir", "Ali"]);
+
+    // Example : 2
+    let name = ref(["khan", "malik"]);
 </script>
 
 <template>
+    <!-- Example-1 -->
     <p v-for="person in people">{{ person }}</p>
+
+    <!-- Example-2 -->
+    <p v-for=" (names,index) in name" :key="index">{{index}}  {{names}} </p>
 </template>
 
 <style scoped></style>
