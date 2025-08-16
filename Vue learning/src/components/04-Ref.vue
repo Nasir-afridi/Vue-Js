cb5f0a59579203c9f7521cc7d7a24f876651f58b
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 16:59:55 +0500

    Example 1 simple counter

---
+++ Vue learning/src/components/04-Ref.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { ref } from 'vue';
+
+    let count = ref(0)
+</script>
+
+<template>
+    <h1>Count is : {{ count }}</h1>
+    <button @click="count++">+</button>
+    <button @click="count--">-</button>
+</template>
+
+<style scoped></style>
