42016e420e7a71657c4f77737e47e69b79e1f37f
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 22:55:08 +0500

    import the Conditional Rendering component

--- Vue learning/src/components/HelloWorld.vue
+++ Vue learning/src/components/HelloWorld.vue
@@ -3,7 +3,8 @@
     import AttributeBinding from './02-AttributeBinding.vue';
     import Reactive from './03-Reactive.vue';
     import Ref from './04-Ref.vue';
-    import Computed from './05-Computed.vue'
+    import Computed from './05-Computed.vue';
+    import ConditionalRendering from './06-ConditionalRendering.vue';
 </script>
 
 
