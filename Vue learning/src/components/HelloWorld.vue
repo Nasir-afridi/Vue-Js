3b55f8ab0510489e63b4fbee87104c17ceb99c24
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 16:59:33 +0500

    import the Ref function

--- Vue learning/src/components/HelloWorld.vue
+++ Vue learning/src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
     import TextInterpolation from './01-TextInterpolation.vue';
     import AttributeBinding from './02-AttributeBinding.vue';
     import Reactive from './03-Reactive.vue';
+    import Ref from './04-Ref.vue';
 </script>
 
 
@@ -10,6 +11,7 @@
     <TextInterpolation />
     <AttributeBinding />
     <Reactive /> 
+    <Ref />
 </template>
 
 
