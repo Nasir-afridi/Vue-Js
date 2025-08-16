1e0d4a37774d4f4bcf904a3f8aa0fd5e3c41b5d9
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 17:05:21 +0500

    added the example 2 a button when press is its show the name

--- Vue learning/src/components/04-Ref.vue
+++ Vue learning/src/components/04-Ref.vue
@@ -1,13 +1,22 @@
 <script setup>
     import { ref } from 'vue';
-
+    // 1.
     let count = ref(0)
+
+    // 2.
+    let userName = ref('')
 </script>
 
 <template>
+    <!-- 1 -->
     <h1>Count is : {{ count }}</h1>
     <button @click="count++">+</button>
     <button @click="count--">-</button>
+
+
+    <!-- 2 -->
+     <h1>UserName is {{ userName }}</h1>
+    <button @click="userName = 'Nasir'">Add Name</button>
 </template>
 
 <style scoped></style>
