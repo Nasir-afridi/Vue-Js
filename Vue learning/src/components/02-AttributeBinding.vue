de96d3e07bb7ac527dd1abdbc1f99700e074c836
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 16:43:44 +0500

    Attribute binding : means link the data with html

---
+++ Vue learning/src/components/02-AttributeBinding.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const myChannel = "https://www.youtube.com/watch?v=NcnfsE1B51o&list=PLEPye7A7EcQaJupAt4yTtJx3K-uUuj8si&index=3&ab_channel=SuperSimpleDev"
+    const Image = "/favicon.ico"
+    const ImageWidth = 400
+    const ImageHeight = 400
+</script>
+
+<template>
+    <a :href="myChannel">Youtube channel</a>       
+    <img :src="Image" :width="ImageWidth" :height="ImageHeight" >
+</template>
+
+<style scoped></style>
