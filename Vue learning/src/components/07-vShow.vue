9c5f1add06778c5fb65c393e8eab4c15a872a8a0
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 23:37:55 +0500

    Added the vShow component to show or hide messages

---
+++ Vue learning/src/components/07-vShow.vue
@@ -0,0 +1,11 @@
+<!-- vShow dom mein hamesha addd rhta hai beshak true ho ya false agrr true hai to show hoga agrr false hai to hide hoga -->
+
+<script setup>
+    let showMessage = false;
+</script>
+
+<template>
+    <p v-show="showMessage">This is Paragraph</p>
+</template>
+
+<style scoped></style>
