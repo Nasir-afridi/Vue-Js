0f1e3bfbac0ff78169673f883051114031fbed01
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 16:42:34 +0500

    Added the text-interpolation in which we use js inside html elements

---
+++ Vue learning/src/components/01-TextInterpolation.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const myMessage = 'Yellow Color';
+    function add (a, b ) {
+        return a + b
+    }
+</script>
+
+<template>
+    <h1>     
+        Hello World {{ myMessage }}
+        {{ 2 + 2 }} = {{ 22 + 22 }}
+        <br>
+        {{ add(2, 4)}}
+    </h1>
+</template>
+
+<style scoped></style>
