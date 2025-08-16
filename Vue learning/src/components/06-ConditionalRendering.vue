7810e886da8050a256dda94316af8b00e747d5bc
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 23:29:01 +0500

    added the condtional rendering v-if, v-else-if, and v-else

---
+++ Vue learning/src/components/06-ConditionalRendering.vue
@@ -0,0 +1,16 @@
+<!-- if else-if else DOM mein tbhe ainge jbb wo true honge agrr wo false hain to dom mein nahe aainge -->
+
+<script setup>
+    const isTrue = false;
+    const isFalse = false;
+</script>
+
+
+<template>
+    <p v-if="isTrue">This is will something</p>
+    <p v-else-if="isFalse">show me something</p>
+    <p v-else>Nothing is true as false.</p>
+</template>
+
+
+<style scoped></style>
