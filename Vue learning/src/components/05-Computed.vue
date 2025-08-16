c1024564ab656ed8fb95d91614fb0015a3f68e9f
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 20:52:42 +0500

    added the computed component with ref

---
+++ Vue learning/src/components/05-Computed.vue
@@ -0,0 +1,23 @@
+
+
+<!-- Computed: do ya do sy zyada ref / reactive ko aik banana jaisy first name + lastname ko aik final fullName variable mein lana -->
+
+
+
+<script setup>
+    import { computed, ref } from 'vue';
+
+    const firstName = ref("Nasir");
+    const lastName = ref("khan");
+    const fullName = computed(() => firstName.value + " " + lastName.value)
+</script>
+
+
+
+<template>
+    <h1>Full Name is {{ fullName }}</h1>
+</template>
+
+
+
+<style scoped></style>
