1784b7478a77b817e710981e00406e068cca0ffd
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 23:44:16 +0500

    Added the button to to hide or display the paragraph using ref

--- Vue learning/src/components/07-vShow.vue
+++ Vue learning/src/components/07-vShow.vue
@@ -1,11 +1,13 @@
 <!-- vShow dom mein hamesha addd rhta hai beshak true ho ya false agrr true hai to show hoga agrr false hai to hide hoga -->
 
 <script setup>
-    let showMessage = false;
+    import { ref } from 'vue';
+    let showMessage = ref(true);
 </script>
 
 <template>
     <p v-show="showMessage">This is Paragraph</p>
+    <button @click="showMessage = !showMessage">toggle button</button>
 </template>
 
 <style scoped></style>
