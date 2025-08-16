3903a4d0ae5f332d7c4800f6b2f45f5d61258332
Nasir-afridi <nasirwc05@gmail.com>
2025-08-16 21:03:08 +0500

    Comment added for the information

--- Vue learning/src/components/02-AttributeBinding.vue
+++ Vue learning/src/components/02-AttributeBinding.vue
@@ -1,3 +1,6 @@
+<!-- Data ko html sy connect krny kk liye hota hia takay data mein koi change ho to html osko foraan trigger
+ kray orrr ui update krday yane hamain html mein change na krna paray -->
+
 <script setup>
     const myChannel = "https://www.youtube.com/watch?v=NcnfsE1B51o&list=PLEPye7A7EcQaJupAt4yTtJx3K-uUuj8si&index=3&ab_channel=SuperSimpleDev"
     const Image = "/favicon.ico"
