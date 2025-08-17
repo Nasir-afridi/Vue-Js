7ecb5764d037f37929f14add3ae0dff160acfff9
Nasir-afridi <nasirwc05@gmail.com>
2025-08-17 15:29:22 +0500

    added the FallBack Content component means if the slots does'nt have any content it will pick the by default content

---
+++ Vue learning/src/components/12-FallbackContentSlots.vue
@@ -0,0 +1,12 @@
+<!-- 
+    fallback content by default wala content hota hai yane agrr ham ny slots mein kuch content nahe diya
+    to wo by default wala  content pick krlyga
+-->
+
+<script setup></script>
+
+<template>
+    <slot><h1>default text of slot because we dont pass the data from the parent so it pick this data</h1></slot>
+</template>
+
+<style scoped></style>
