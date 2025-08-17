b20d142fb03defd86d4e0d7b01ac9e4855607f23
Nasir-afridi <nasirwc05@gmail.com>
2025-08-17 14:55:49 +0500

    Slots added with comment

---
+++ Vue learning/src/components/11-Slots.vue
@@ -0,0 +1,10 @@
+<!-- slots kk zarye ham html content ko bhaij skty hain jaisy props sy data bhaijty hain 
+ waisy he slots syy html content   -->
+
+<script setup></script>
+
+<template>
+    <slot></slot>
+</template>
+
+<style scoped></style>
