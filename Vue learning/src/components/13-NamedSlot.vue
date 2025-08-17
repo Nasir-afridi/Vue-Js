c9133941c0882870fdefa89f0b2ed5a27a192549
Nasir-afridi <nasirwc05@gmail.com>
2025-08-18 00:07:57 +0500

    Named slot component added

---
+++ Vue learning/src/components/13-NamedSlot.vue
@@ -0,0 +1,10 @@
+<!-- Named slot ka mtlb hai kkk slot ko aik specific name dydo jaisy ham css mein class daity hain. -->
+
+<script setup>
+
+</script>
+
+<template>
+    <slot name="one"></slot>
+    <slot name="two"></slot>
+</template>
