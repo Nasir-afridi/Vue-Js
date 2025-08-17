658b1e4ab649e95a478f8e5291ab58e36ec6d356
Nasir-afridi <nasirwc05@gmail.com>
2025-08-17 13:27:59 +0500

    Sending the simple name from the parent component to child Props component

---
+++ Vue learning/src/components/09-Props.vue
@@ -0,0 +1,14 @@
+<!-- sending data from the parent component to the child component -->
+
+<script setup>
+    let value = defineProps(['name'])
+    console.log(value)
+</script>
+
+
+<template>
+    <h1>{{ value.name }}</h1>
+</template>
+
+
+<style scoped></style>
