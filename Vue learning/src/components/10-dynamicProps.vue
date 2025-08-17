9ce814129668f807ba4441371ff7a593f48eadf5
Nasir-afridi <nasirwc05@gmail.com>
2025-08-17 13:39:27 +0500

    receiving data from the parent component

---
+++ Vue learning/src/components/10-dynamicProps.vue
@@ -0,0 +1,14 @@
+<script setup>
+    // Example-1
+    const value = defineProps(['firstName', 'lastName'])
+</script>
+
+
+<template>
+    <!-- Example-1 -->
+    <h1>First Name : {{ value.firstName }}</h1>
+    <h1>Last Name : {{ value.lastName }}</h1>
+</template>
+
+j
+<style scoped></style>
