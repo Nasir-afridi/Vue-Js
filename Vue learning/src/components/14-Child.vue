f2b2aae4842e310df87216850e9e7c041485ba5d
Nasir-afridi <nasirwc05@gmail.com>
2025-08-18 18:32:39 +0500

    added the comment and example-02 inject the data from the grandparent

--- Vue learning/src/components/14-Child.vue
+++ Vue learning/src/components/14-Child.vue
@@ -1,18 +1,33 @@
-   <!-- ye apny parent Provideinject sy data receive kry using props
-    yane grandparent nyy apny child ko diya pher os child nyy apny child ko diya. -->
+<!-- 
+   ye apny parent Provideinject sy data receive kry using props
+   yane grandparent nyy apny child ko diya pher os child nyy apny child ko diya. 
+-->
+
    <script setup>
+      // Example-1 without using provide and inject
         defineProps([
             'studentName',
             'studentAge', 
             'studentLocation'
-
         ])
-    </script>
+
+      // Example-2 using provide and inject
+      import { inject } from 'vue';
+      const name = inject('firstName')
+      const age = inject('age')
+      const location = inject('location')
+   </script>
 
  <template>
+   <!-- Example-1 without using provide and inject -->
     <h1>{{ studentName }}</h1>
     <h2>{{ studentAge }}</h2>
     <h3>{{ studentLocation }}</h3>
+
+    <!-- Example-2 using provide and inject -->
+    <h1>{{ name }}</h1>
+    <h2>{{ age }}</h2>
+    <h3>{{ location }}</h3>
  </template>
 
  <style scoped></style>
