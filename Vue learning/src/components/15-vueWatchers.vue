0bc15b28d458d361580f6bab5a66db0dd62c2142
Nasir-afridi <nasirwc05@gmail.com>
2025-08-18 18:49:15 +0500

    Added comments explaining how Vue watchers work

---
+++ Vue learning/src/components/15-vueWatchers.vue
@@ -0,0 +1,20 @@
+<!-- 
+ Watcher : 
+Data ko watch karta hai (matlab reactive variable / prop)
+
+Agar us data me change aata hai → turant react karta hai
+
+Aur tumhari likhi hui function / logic run kar deta hai 
+-->
+
+<script setup>
+    
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
