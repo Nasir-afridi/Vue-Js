84346ff4ec30f610105796c93236011338e364f4
Nasir-afridi <nasirwc05@gmail.com>
2025-08-18 19:22:04 +0500

    live input changing in watcher component

--- Vue learning/src/components/15-vueWatchers.vue
+++ Vue learning/src/components/15-vueWatchers.vue
@@ -8,11 +8,19 @@ Aur tumhari likhi hui function / logic run kar deta hai
 -->
 
 <script setup>
-    
+    import { watch, ref } from 'vue';
+    const message = ref("hello")
+    const input = ref('')
+
+
+    watch(input, (newValue, oldValue) => {
+        message.value = `You typed : ${newValue}`
+    })
 </script>
 
 <template>
-
+    <h1>{{ message }}</h1>
+    <input v-model="input" placeholder="type something here" type="text">
 </template>
 
 <style scoped>
