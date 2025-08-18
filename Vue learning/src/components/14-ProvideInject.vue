8eff69e72dd25149b3e8edcf39fff30572366a71
Nasir-afridi <nasirwc05@gmail.com>
2025-08-18 18:37:41 +0500

    sending data to the grand child using props and also using Provide & Inject

---
+++ Vue learning/src/components/14-ProvideInject.vue
@@ -0,0 +1,20 @@
+<!-- ismein ham grand parent syyy direct child ko data bhaij skty hain yane parent kk child kk child ko direct -->
+ <script setup>
+    import Child from './14-Child.vue'
+    defineProps([
+        'studentName',
+        'studentAge', 
+        'studentLocation'
+
+    ])
+</script>
+
+ <template>
+    <Child 
+        :studentName = "studentName"
+        :studentAge = "studentAge"
+        :studentLocation = "studentLocation"
+    />
+ </template>
+
+ <style scoped></style>
